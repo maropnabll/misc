899a1e25f1a42c33ea35434e7b83e2c4ef14005b
bus <19573092+maropnabll@users.noreply.github.com>
2024-06-15 21:29:47 -0400

    Create swing.lua

---
+++ animations/swing.lua
@@ -0,0 +1,4 @@
+-- // converted using mech's animoduler
+local cfr = CFrame.new
+
+return {{tm=0;["EnableTrail"]="";["Torso"]={tm=.25;cf=cfr(0,0,0,1,0,0,0,1,0,0,0,1);es="Linear";ed="In";};["Head"]={tm=.25;cf=cfr(0,0,0,1,0,0,0,1,0,0,0,1);es="Linear";ed="In";};["Right Arm"]={tm=.25;cf=cfr(0,0,0,1,0,0,0,1,0,0,0,1);es="Linear";ed="In";};};{tm=.25;["PauseAndFlash"]="";["Torso"]={tm=.0830;cf=cfr(0,0,0,.906,.423,0,-.423,.906,0,0,0,1);es="Linear";ed="In";};["Head"]={tm=.0830;cf=cfr(0,0,0,.902,-.432,0,.432,.902,0,0,0,1);es="Linear";ed="In";};["Right Arm"]={tm=.0830;cf=cfr(-.644,-.192,-.239,-.883,.464,-.074,-.463,-.886,-.032,-.08,.006,.997);es="Linear";ed="In";};};{tm=.333;["DisableTrail"]="";["Torso"]={tm=0;cf=cfr(0,0,0,.819,-.574,0,.574,.819,0,0,0,1);es="Linear";ed="In";};["Head"]={tm=0;cf=cfr(0,0,0,.828,.561,0,-.561,.828,0,0,0,1);es="Linear";ed="In";};["Right Arm"]={tm=0;cf=cfr(.971,-.192,-.97,.62,-.762,.187,.497,.196,-.845,.608,.617,.5);es="Linear";ed="In";};};}
