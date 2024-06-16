140b3143bf06be855971924ac771e2dd1a3b2b63
bus <19573092+maropnabll@users.noreply.github.com>
2024-06-15 21:29:16 -0400

    Create idle.lua

---
+++ animations/idle.lua
@@ -0,0 +1,4 @@
+-- // converted using mech's animoduler
+local cfr = CFrame.new
+
+return {{tm=0;["Right Arm"]={tm=10;cf=cfr(.125,-.623,-.08,.277,-.896,.346,.928,.343,.146,-.249,.281,.927);es="Linear";ed="In";};};{tm=10;["Right Arm"]={tm=0;cf=cfr(.125,-.623,-.08,.277,-.896,.346,.928,.343,.146,-.249,.281,.927);es="Linear";ed="In";};};}
